100a7c586a149d330b3011f3b4056fe905bd6649
lorenalopesm <lore.lopees@outlook.com>
2022-11-22 20:45:01 -0300

    :tada: initial commit

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-<h1 align="center"> NLW #10 Copa </h1>
+<h1 align="center"> NLW Copa 2022 </h1>
 
 <p align="center">
 Esse projeto foi desenvolvido no evento Next Level Week(NLW)- Copa, em sua décima edição, o qual foi exclusivo e gratuito, promovido pela Rocketseat para ensino de tecnologias WEB.
@@ -15,11 +15,12 @@ Esse projeto foi desenvolvido no evento Next Level Week(NLW)- Copa, em sua déci
   <img alt="License" src="https://img.shields.io/static/v1?label=license&message=MIT&color=49AA26&labelColor=000000">
 </p>
 
-<br>
 
-<p align="center">
-  <img alt="calendario da copa" src=".github/preview.jpg" width="100%">
-</p>
+
+
+## 🛠 Atualizações feitas:
+
+Implementei uma função para trocar o tema da tela através do botão Alterar Tema, como também, o nome dos países embaixo das suas bandeiras.
 
 ## 🚀 Tecnologias
 
@@ -31,12 +32,16 @@ Esse projeto foi desenvolvido com as seguintes tecnologias:
 
 ## 💻 Projeto
 
-O Calendário da Copa é um projeto que mostra os jogos da Copa do Qatar, 2022.
+O Calendário da Copa tem o intuito de mostra as datas, horários e equipes que jogarão na primeira fase da Copa do Mundo do Qatar, 2022.
 
 ## 🔖 Layout
 
-Você pode visualizar o layout do projeto através [DESSE LINK](https://www.figma.com/file/J1Z33MISC22YZB8wfxiIns/NLW-Copa-Explorer/duplicate). É necessário ter conta no [Figma](https://figma.com) para acessá-lo.
+Você pode visualizar o layout do projeto através [DESSE LINK](https://www.figma.com/file/TV8k4cTr9Jdo4s4Wmz9EyU/Calend%C3%A1rio-de-Jogos-(Community)?node-id=0%3A1). É necessário ter conta no [Figma](https://figma.com) para acessá-lo.
 
-## 📑 Licença
+## :memo: Licença
 
 Esse projeto está sob a licença MIT.
+
+---
+
+Feito com ♥ by Rocketseat e lorenalopesm :wave: [Participe da nossa comunidade!](https://discord.gg/rocketseat)
--- main.js
+++ main.js
@@ -1,51 +1,131 @@
 function createGame(player1, hour, player2) {
   return `
   <li>
-    <img src="./assets/icon-${player1}.svg" alt="Bandeira do(a) ${player1}">
+    <div class="country">
+      <img src="./assets/${player1}-icon.svg" alt="ícone redondo da bandeira do ${player1}">
+      <p>${player1}</p>
+    </div>
     <strong>${hour}</strong>
-    <img src="./assets/icon-${player2}.svg" alt="Bandeira do(a) ${player2}">
-  </li>
+    <div class="country">
+      <img src="./assets/${player2}-icon.svg" alt="ícone redondo da bandeira de ${player2}">
+      <p>${player2}</p>
+    </div>
+</li>
   `
 }
-let delay = -0.3;
-function creatCard(date, day, games) {
+
+let delay = -0.3
+function createCard(date, day, games) {
   delay = delay + 0.3
   return `
-   <div class="card" style="animation-delay: ${delay}s">
-      
-      <h2>${date} <span>${day}</span></h2>
-        <ul>
-          ${games}
-        </ul>
-    </div>
+  <div class="card" style="animation-delay:${delay}s">
+    <h2>${date} <span>${day}</span></h2>
+
+    <ul>
+     ${games}
+    </ul>
+  </div>
   `
 }
 
-document.querySelector("#cards").innerHTML = 
+function changeTheme() {
+  document.body.classList.toggle("blue")0
+}
 
-   creatCard(
-     "24/11",
-     "Quinta",
-     createGame("switzerland", "16:00", "camaroon") +
-       createGame("koreia", "16:00", "uruguay") +
-       createGame("portugal", "13:00", "ghana") +
-       createGame("brazil", "16:00", "servia")
-   )  +    
-   creatCard(
-     "28/11",
-     "Segunda",
-     createGame("camaroon", "07:00", "servia") +
-       createGame("koreia", "10:00", "ghana") +
-       createGame("brazil", "13:00", "switzerland") +
-       createGame("portugal", "16:00", "uruguay")
-   )  +   
-   creatCard(
-     "02/12",
-     "Sexta",
-     createGame("koreia", "12:00", "portugal") +
-     createGame("ghana", "12:00", "uruguay") +
-     createGame("brazil", "16:00", "camaroon") +
-     createGame("servia", "16:00", "switzerland")
-   )      
-  
-
+document.querySelector("#cards").innerHTML =
+  createCard("20/11", "domingo", createGame("qatar", "16:00", "equador")) +
+  createCard(
+    "21/11",
+    "segunda",
+    createGame("inglaterra", "10:00", "iran") +
+      createGame("senegal", "13:00", "holanda") +
+      createGame("estadosUnidos", "16:00", "paisDeGales")
+  ) +
+  createCard(
+    "22/11",
+    "terça",
+    createGame("argentina", "07:00", "arabiaSaudita") +
+      createGame("dinamarca", "10:00", "tunisia") +
+      createGame("mexico", "13:00", "polonia") +
+      createGame("australia", "16:00", "franca")
+  ) +
+  createCard(
+    "23/11",
+    "quarta",
+    createGame("marrocos", "07:00", "croacia") +
+      createGame("alemanha", "10:00", "japao") +
+      createGame("espanha", "13:00", "costaRica") +
+      createGame("belgica", "16:00", "canada")
+  ) +
+  createCard(
+    "24/11",
+    "quinta",
+    createGame("camaroes", "07:00", "suica") +
+      createGame("coreiaDoSul", "10:00", "uruguai") +
+      createGame("portugal", "13:00", "gana") +
+      createGame("brasil", "16:00", "servia")
+  ) +
+  createCard(
+    "25/11",
+    "sexta",
+    createGame("paisDeGales", "07:00", "iran") +
+      createGame("qatar", "10:00", "senegal") +
+      createGame("holanda", "13:00", "equador") +
+      createGame("inglaterra", "16:00", "estadosUnidos")
+  ) +
+  createCard(
+    "26/11",
+    "sábado",
+    createGame("tunisia", "07:00", "australia") +
+      createGame("polonia", "10:00", "arabiaSaudita") +
+      createGame("franca", "13:00", "dinamarca") +
+      createGame("argentina", "16:00", "mexico")
+  ) +
+  createCard(
+    "27/11",
+    "domingo",
+    createGame("japao", "07:00", "costaRica") +
+      createGame("belgica", "10:00", "marrocos") +
+      createGame("croacia", "13:00", "canada") +
+      createGame("alemanha", "16:00", "espanha")
+  ) +
+  createCard(
+    "28/11",
+    "segunda",
+    createGame("servia", "07:00", "camaroes") +
+      createGame("coreiaDoSul", "10:00", "gana") +
+      createGame("brasil", "13:00", "suica") +
+      createGame("portugal", "16:00", "uruguai")
+  ) +
+  createCard(
+    "29/11",
+    "terça",
+    createGame("equador", "12:00", "senegal") +
+      createGame("holanda", "12:00", "qatar") +
+      createGame("iran", "16:00", "estadosUnidos") +
+      createGame("paisDeGales", "16:00", "inglaterra")
+  ) +
+  createCard(
+    "30/11",
+    "quarta",
+    createGame("tunisia", "12:00", "franca") +
+      createGame("australia", "12:00", "dinamarca") +
+      createGame("polonia", "16:00", "argentina") +
+      createGame("arabiaSaudita", "16:00", "mexico")
+  ) +
+  createCard(
+    "01/12",
+    "quinta",
+    createGame("croacia", "12:00", "belgica") +
+      createGame("canada", "12:00", "marrocos") +
+      createGame("japao", "16:00", "espanha") +
+      createGame("costaRica", "16:00", "alemanha")
+  ) +
+  createCard(
+    "02/12",
+    "sexta",
+    createGame("coreiaDoSul", "12:00", "portugal") +
+      createGame("gana", "12:00", "uruguai") +
+      createGame("servia", "16:00", "suica") +
+      createGame("camaroes", "16:00", "brasil")
+  )
